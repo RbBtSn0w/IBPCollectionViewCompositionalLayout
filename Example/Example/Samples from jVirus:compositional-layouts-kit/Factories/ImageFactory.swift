4b284f0492ce1cec9607c058bf32c647f7786cac
rbbtsn0w <hamiltonsnow@gmail.com>
2021-10-19 20:07:16 +0800

    Create image below iOS 10

--- Example/Example/Samples from jVirus:compositional-layouts-kit/Factories/ImageFactory.swift
+++ Example/Example/Samples from jVirus:compositional-layouts-kit/Factories/ImageFactory.swift
@@ -28,7 +28,13 @@ struct ImageFactory {
             })
         } else {
             // Fallback on earlier versions
-            return nil
+            UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+            UIColor.cornflowerBlue.setFill()
+            UIRectFill(CGRect(origin: CGPoint.zero, size: size))
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            guard let cgImage = image?.cgImage else { return nil }
+            return UIImage(cgImage: cgImage)
         }
     }
     
